c7d664ff29ac3804866760b3376af26d22eb4b19
Alberto Basalo <albertobasalo@hotmail.com>
2022-11-10 11:14:00 +0100

    feat: agencies list

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,39 +3,51 @@ import { Component } from "@angular/core";
 @Component({
   selector: "app-root",
   template: `
-    <!--The content below is only a placeholder and can be replaced.-->
-    <div style="text-align:center" class="content">
-      <h1>Welcome to {{ title }}!</h1>
-      <span style="display: block">{{ title }} app is running!</span>
-    </div>
-    <h2>Here are some links to help you start:</h2>
-    <ul>
-      <li>
-        <h2>
-          <a target="_blank" rel="noopener" href="https://angular.io/tutorial">
-            Tour of Heroes
-          </a>
-        </h2>
-      </li>
-      <li>
-        <h2>
-          <a target="_blank" rel="noopener" href="https://angular.io/cli">
-            CLI Documentation
-          </a>
-        </h2>
-      </li>
-      <li>
-        <h2>
-          <a target="_blank" rel="noopener" href="https://blog.angular.io/">
-            Angular blog
-          </a>
-        </h2>
-      </li>
-    </ul>
-    <router-outlet></router-outlet>
+    <header>
+      <strong>{{ title | uppercase }}</strong>
+    </header>
+    <main>
+      <h3>Working with {{ agencies.length }} space agencies</h3>
+      <ul>
+        <li *ngFor="let agency of agencies">
+          <span [class]="agency.status | lowercase">{{ agency.name }}</span>
+          ➖
+          <span *ngIf="agency.range === 'Orbital'">{{ agency.range }} 🌍</span>
+          <span *ngIf="agency.range !== 'Orbital'">{{ agency.range }} 🪐</span>
+        </li>
+      </ul>
+      <router-outlet></router-outlet>
+    </main>
+    <footer>
+      <p>
+        <a href="{{ repoUrl }}" target="_blank">
+          {{ title }}
+        </a>
+        made by
+        <a [href]="authorUrl" target="_blank">{{ author }}</a>
+      </p>
+    </footer>
   `,
-  styles: [],
+  styles: [
+    `
+      .active {
+        color: green;
+      }
+      .pending {
+        color: orange;
+      }
+    `,
+  ],
 })
 export class AppComponent {
-  title = "altia-angular_intro-noviembre";
+  title = "Altia noviembre";
+  author = "Alberto Basalo";
+  repoUrl =
+    "https://github.com/TrainingITCourses/altia-angular_intro-noviembre";
+  authorUrl = "https://albertobasalo.dev";
+  agencies = [
+    { name: "Space X", range: "Interplanetary", status: "Active" },
+    { name: "Blue Origin", range: "Orbital", status: "Active" },
+    { name: "Virgin Galactic", range: "Orbital", status: "Pending" },
+  ];
 }

faca08dadde13df2b360216fc612e8cf58993703
Alberto Basalo <albertobasalo@hotmail.com>
2022-11-15 12:19:48 +0100

    feat: agencies trips component

---
+++ src/app/home/agencies.component.ts
@@ -0,0 +1,49 @@
+import { Component, OnInit } from "@angular/core";
+
+@Component({
+  selector: "app-agencies",
+  template: `
+    <h3>Working with {{ agencies.length }} space agencies</h3>
+    <section>
+      <button (click)="loadAgencies()">All agencies</button>
+      <button (click)="loadFilteredAgencies()">Only actives</button>
+    </section>
+    <ul>
+      <li *ngFor="let agency of agencies">
+        <span [class]="agency.status | lowercase">{{ agency.name }}</span>
+        <span *ngIf="agency.range === 'Orbital'">{{ agency.range }} 🌍</span>
+        <span *ngIf="agency.range !== 'Orbital'">{{ agency.range }} 🪐</span>
+      </li>
+    </ul>
+  `,
+  styles: [
+    `
+      .active {
+        color: green;
+      }
+      .pending {
+        color: orange;
+      }
+    `,
+  ],
+})
+export class AgenciesComponent implements OnInit {
+  agencies: any[] = [];
+  agenciesRaw = [
+    { name: "Space X", range: "Interplanetary", status: "Active" },
+    { name: "Blue Origin", range: "Orbital", status: "Active" },
+    { name: "Virgin Galactic", range: "Orbital", status: "Pending" },
+  ];
+  constructor() {
+    this.loadAgencies();
+  }
+
+  loadAgencies() {
+    this.agencies = this.agenciesRaw;
+  }
+  loadFilteredAgencies() {
+    this.agencies = this.agenciesRaw.filter((a) => a.status === "Active");
+  }
+
+  ngOnInit(): void {}
+}
--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -1,9 +1,11 @@
 import { CommonModule } from "@angular/common";
 import { NgModule } from "@angular/core";
 import { HomeComponent } from "./home.component";
+import { AgenciesComponent } from './agencies.component';
+import { TripsComponent } from './trips.component';
 
 @NgModule({
-  declarations: [HomeComponent],
+  declarations: [HomeComponent, AgenciesComponent, TripsComponent],
   imports: [CommonModule],
   exports: [HomeComponent],
 })

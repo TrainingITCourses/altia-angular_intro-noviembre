a2854d3200c1493b51a57be7f86470d148c87d2d
Alberto Basalo <albertobasalo@hotmail.com>
2022-11-29 11:01:23 +0100

    feat: input output

--- src/app/home/agencies.component.ts
+++ src/app/home/agencies.component.ts
@@ -1,14 +1,13 @@
-import { Component, OnInit } from "@angular/core";
+import { Component, EventEmitter, Input, OnInit, Output } from "@angular/core";
 import { Agency } from "../core/models/agency.interface";
-import { ApiService } from "../core/services/api.service";
 
 @Component({
   selector: "app-agencies",
   template: `
     <h3>Working with {{ agencies.length }} space agencies</h3>
     <section>
-      <button (click)="loadAgencies()">All agencies</button>
-      <button (click)="loadFilteredAgencies()">Only actives</button>
+      <button (click)="onClick('all')">All agencies</button>
+      <button (click)="onClick('active')">Only actives</button>
     </section>
     <ul>
       <li *ngFor="let agency of agencies">
@@ -30,25 +29,13 @@ import { ApiService } from "../core/services/api.service";
   ],
 })
 export class AgenciesComponent implements OnInit {
-  agencies: Partial<Agency>[] = [];
-  agenciesRaw: Agency[] = [];
-
-  constructor(private api: ApiService) {
-    this.loadAgencies();
-  }
-
-  loadAgencies() {
-    console.log("Loading agencies...");
-    this.api.getAgencies$().subscribe((agencies) => {
-      this.agenciesRaw = agencies;
-      this.agencies = agencies;
-      console.log("1️⃣ antes en el espacio agencies: " + this.agencies.length);
-    });
-    console.log("0️⃣ antes en el tiempo agencies: " + this.agencies.length);
-  }
-  loadFilteredAgencies() {
-    this.agencies = this.agenciesRaw.filter((a) => a.status === "Active");
-  }
+  @Input() agencies: Partial<Agency>[] = [];
+  @Output() load: EventEmitter<string> = new EventEmitter();
 
   ngOnInit(): void {}
+
+  onClick(buttonCaption: string) {
+    console.log("event click", buttonCaption);
+    this.load.next(buttonCaption);
+  }
 }
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,13 +1,47 @@
 import { Component, OnInit } from "@angular/core";
+import { Agency } from "../core/models/agency.interface";
+import { ApiService } from "../core/services/api.service";
 
 @Component({
   selector: "app-home",
   template: `
-    <app-agencies></app-agencies>
+    <app-agencies
+      [agencies]="agenciesApi"
+      (load)="onLoad($event)"></app-agencies>
     <app-trips></app-trips>
   `,
   styles: [],
 })
 export class HomeComponent implements OnInit {
+  agenciesApi: Partial<Agency>[] = [];
+  agenciesRawApi: Agency[] = [];
+
+  constructor(private api: ApiService) {
+    this.loadAgencies();
+  }
+
   ngOnInit(): void {}
+
+  loadAgencies() {
+    console.log("Loading agencies...");
+    this.api.getAgencies$().subscribe((agencies) => {
+      this.agenciesRawApi = agencies;
+      this.agenciesApi = agencies;
+      console.log(
+        "1️⃣ antes en el espacio agencies: " + this.agenciesApi.length
+      );
+    });
+    console.log("0️⃣ antes en el tiempo agencies: " + this.agenciesApi.length);
+  }
+
+  onLoad(filter: string) {
+    console.log("event on load ", filter);
+    if (filter === "all") {
+      this.agenciesApi = [...this.agenciesRawApi];
+    } else {
+      this.agenciesApi = this.agenciesRawApi.filter(
+        (a) => a.status === "Active"
+      );
+    }
+  }
 }
